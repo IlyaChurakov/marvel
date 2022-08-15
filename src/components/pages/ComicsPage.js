d34682d68ffc17e3c5bc40809ba96cec8037ab91
Ilya <churakov018@gmail.com>
2022-08-15 09:53:55 +0300

    commit 3

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import AppBanner from '../appBanner/AppBanner';
+import ComicsList from '../comicsList/ComicsList'
+
+const ComicsPage = () => {
+
+    const [selectedComic, setSelectedComic] = useState(null);
+
+    const onComicsSelected = (id) => {
+        setSelectedComic(id);
+    }
+
+    return (
+        <>
+            <AppBanner/>
+            <ComicsList onComicsSelected={onComicsSelected}/>
+        </>
+    )
+}
+
+export default ComicsPage
